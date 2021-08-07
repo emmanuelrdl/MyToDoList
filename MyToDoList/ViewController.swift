712889ce4f13b492d8d7aec0d508ec9d474d26c3
emmanuel rudelle <emmanuel.rudelle@hotmail.fr>
2021-08-07 18:03:20 +0200

    Initial Commit

---
+++ MyToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDoList
+//
+//  Created by emmanuel rudelle on 07/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
